d8d45b99bc9b4c3dc0f6aa4f59e23337fc512c26
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-03-29 00:11:01 +0200

    siteurl added

--- dataversedock/dara.bash
+++ dataversedock/dara.bash
@@ -8,5 +8,6 @@ if [ "$doiprovider" == "dara-4.0" ]; then
     curl -X PUT -d $authority http://localhost:8080/api/admin/settings/:Authority
     curl -X PUT -d $DOIseparator http://localhost:8080/api/admin/settings/:DoiSeparator
     /opt/glassfish4/glassfish/bin/asadmin create-jvm-options '-Ddoi.baseurlstring="$baseuri"'
+    /opt/glassfish4/glassfish/bin/asadmin create-jvm-options '-Ddataverse.fqdn=test.dataverse.cessda.eu'
     /opt/glassfish4/glassfish/bin/asadmin create-jvm-options -Ddoi.username="$login":-Ddoi.password="$password"
 fi
