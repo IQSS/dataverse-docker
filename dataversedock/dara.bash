382e116c450e21aad511389fa94596a2f77cb5bc
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-04-19 15:08:50 +0200

    Fixed

--- dataversedock/dara.bash
+++ dataversedock/dara.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ "$doiprovider" == "dara-4.0" ]; then
     echo "Initializing da-ra service for PIDs registration..."
+    cd /opt/dv/deps
+    unzip dara-dv-plugin.zip
     curl http://localhost:8080/api/admin/datasetfield/load -X POST --data-binary @/opt/dv/deps/dara40.tsv -H "Content-type: text/tab-separated-values"
     /opt/glassfish4/glassfish/bin/asadmin add-library /opt/dv/deps/dara-dv-plugin/target/dara-plugin-1.0-SNAPSHOT-jar-with-dependencies.jar
     psql -U dvnapp dvndb -h db -f /opt/dv/actionlog.sql
@@ -9,7 +11,7 @@ if [ "$doiprovider" == "dara-4.0" ]; then
     curl -X PUT -d $authority http://localhost:8080/api/admin/settings/:Authority
     curl -X PUT -d $DOIseparator http://localhost:8080/api/admin/settings/:DoiSeparator
     /opt/glassfish4/glassfish/bin/asadmin create-jvm-options '-Ddoi.baseurlstring="http\://labs.da-ra.de"'
-    /opt/glassfish4/glassfish/bin/asadmin create-jvm-options '-Ddataverse.fqdn=test.dataverse.cessda.eu'
+    /opt/glassfish4/glassfish/bin/asadmin create-jvm-options '-Ddataverse.fqdn="$DVNHOST"'
     /opt/glassfish4/glassfish/bin/asadmin delete-jvm-options -Ddoi.username=apitest
     /opt/glassfish4/glassfish/bin/asadmin delete-jvm-options -Ddoi.password=apitest
     /opt/glassfish4/glassfish/bin/asadmin create-jvm-options -Ddoi.username="$login":-Ddoi.password="$password"
