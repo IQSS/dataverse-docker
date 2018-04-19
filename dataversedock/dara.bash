7379d1159b9b9c9ae433b1d3c728defb34e6e3c5
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-04-20 01:25:22 +0200

    Fixed

--- dataversedock/dara.bash
+++ dataversedock/dara.bash
@@ -10,7 +10,7 @@ if [ "$doiprovider" == "dara-4.0" ]; then
     curl -X PUT -d $doiprovider http://localhost:8080/api/admin/settings/:DoiProvider
     curl -X PUT -d $authority http://localhost:8080/api/admin/settings/:Authority
     curl -X PUT -d $DOIseparator http://localhost:8080/api/admin/settings/:DoiSeparator
-    thishost = uname -a|awk '{print $2}'
+    thishost=uname -a|awk '{print $2}'
     /opt/glassfish4/glassfish/bin/asadmin delete-jvm-options -Ddataverse.fqdn=$thishost
     /opt/glassfish4/glassfish/bin/asadmin delete-jvm-options  '-Ddoi.baseurlstring=https\://ezid.cdlib.org'
     /opt/glassfish4/glassfish/bin/asadmin create-jvm-options '-Ddoi.baseurlstring="http\://labs.da-ra.de"'
