1307e4f9f2b970f211e90ad5d24cb81dac9208b2
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-04-19 19:38:37 +0200

    Fixed

--- dataversedock/setupIT.bash
+++ dataversedock/setupIT.bash
@@ -9,12 +9,13 @@ if [ ! -e /opt/dv/status ]; then
 	unzip dvinstall.zip
 	patch -t /opt/dv/dvinstall/install < docker.patch
 	cd /opt/dv/dvinstall
+	/usr/local/glassfish4/glassfish/bin/asadmin start-domain
 	if [ "$doiprovider" == "dara-4.0" ]; then
 		wget https://github.com/Dans-labs/dataverse/releases/download/4.8.6/cessda_citation.tsv -O /opt/dv/dvinstall/data/metadatablocks/citation.tsv
 		wget https://github.com/Dans-labs/dataverse/releases/download/4.8.6/dataverse-4.8.6.war -O /opt/dv/dvinstall/dataverse.war
+		bash /opt/dv/dara.bash
 	fi
 
-	/usr/local/glassfish4/glassfish/bin/asadmin start-domain
 	#HEALTHCHECK CMD curl --fail http://localhost:8080/api/info/version || exit 1
 	size=$(curl -sI http://localhost:8080/api/info/version | grep Content-Length|awk '{print $2}')
   	echo $size
