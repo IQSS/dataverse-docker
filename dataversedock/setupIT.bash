0e859aea30973f1f09337699c887ba1db8c405ca
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-04-20 14:34:54 +0200

    Fixed

--- dataversedock/setupIT.bash
+++ dataversedock/setupIT.bash
@@ -10,11 +10,6 @@ if [ ! -e /opt/dv/status ]; then
 	patch -t /opt/dv/dvinstall/install < docker.patch
 	cd /opt/dv/dvinstall
 	/usr/local/glassfish4/glassfish/bin/asadmin start-domain
-	if [ "$doiprovider" == "dara-4.0" ]; then
-		wget https://github.com/Dans-labs/dataverse/releases/download/4.8.6/cessda_citation.tsv -O /opt/dv/dvinstall/data/metadatablocks/citation.tsv
-		wget https://github.com/Dans-labs/dataverse/releases/download/4.8.6/dataverse-4.8.6.war -O /opt/dv/dvinstall/dataverse.war
-		bash /opt/dv/dara.bash
-	fi
 
 	#HEALTHCHECK CMD curl --fail http://localhost:8080/api/info/version || exit 1
 	size=$(curl -sI http://localhost:8080/api/info/version | grep Content-Length|awk '{print $2}')
@@ -24,6 +19,12 @@ if [ ! -e /opt/dv/status ]; then
 		echo "Dataverse installation started..."
 	 	./install -mailserver=$MAIL_SERVER -admin_email=$ADMIN_EMAIL -y -f > install.out 2> install.err
 	fi 
+
+        if [ "$doiprovider" == "dara-4.0" ]; then
+                wget https://github.com/Dans-labs/dataverse/releases/download/4.8.6/cessda_citation.tsv -O /opt/dv/dvinstall/data/metadatablocks/citation.tsv
+                wget https://github.com/Dans-labs/dataverse/releases/download/4.8.6/dataverse-4.8.6.war -O /opt/dv/dvinstall/dataverse.war
+                bash /opt/dv/dara.bash
+        fi
 
 	if [ LANG != 'en' ]; then
 		cd /opt/dv/deps
