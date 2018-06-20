78f2f1672b3918bb310abe29b18c1c9df71ca5e1
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-06-20 13:59:56 +0200

    glassfish unzip removed from this step

--- dataversedock/step1.sh
+++ dataversedock/step1.sh
@@ -13,7 +13,6 @@ if [ ! -e $wdir/dv/deps/glassfish4dv.tgz ]; then
 	mkdir -p /tmp/dv-prep/gf
 	cd  $wdir/dv/deps
 	wget http://download.java.net/glassfish/4.1/release/glassfish-4.1.zip
-	unzip glassfish-4.1.zip
 	rm glassfish4/glassfish/modules/weld-osgi-bundle.jar
 	wget http://search.maven.org/remotecontent?filepath=org/jboss/weld/weld-osgi-bundle/2.2.10.Final/weld-osgi-bundle-2.2.10.Final-glassfish4.jar -O weld-osgi-bundle-2.2.10.Final-glassfish4.jar
 	mv weld-osgi-bundle-2.2.10.Final-glassfish4.jar glassfish4/glassfish/modules
