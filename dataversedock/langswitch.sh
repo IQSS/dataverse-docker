173a8de5583b6ea262f2e2809ca33991a12e1e4d
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-07-12 14:02:27 +0200

    Switch adjusted by path

--- dataversedock/langswitch.sh
+++ dataversedock/langswitch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $LANGSWITCH ]; then
-    cp /opt/dv/lang.templates/dataverse_header.xhtml /opt/glassfish4/glassfish/domains/domain1/applications/dataverse/dataverse_header.xhtml
+    cp /opt/dv/dataverse_header.xhtml /opt/glassfish4/glassfish/domains/domain1/applications/dataverse/dataverse_header.xhtml
     echo "header.languages.multi=true"
     echo "header.languages.current=$LANG"
     echo "header.languages.dropdown=$LANGSWITCH"
