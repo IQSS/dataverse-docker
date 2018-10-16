df94d4546f1b0836655d4274b6718c3ae51eaa81
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-10-16 21:51:45 +0200

    Metadata updated

--- dataversedock/dataverse-property-files/languageswitch.sh
+++ dataversedock/dataverse-property-files/languageswitch.sh
@@ -2,7 +2,7 @@
 
 # Compiling bundle properties with metadata
 /usr/local/glassfish4/glassfish/bin/asadmin create-jvm-options "\-Ddataverse.lang.directory=/opt/dv"
-/bin/cp -f /opt/dv/metadataFragment.xhtml /opt/glassfish4/glassfish/domains/domain1/applications/dataverse-4.9.4/metadataFragment.xhtml
+/bin/cp -f /opt/dv/metadataFragment.xhtml /opt/glassfish4/glassfish/domains/domain1/applications/dataverse/metadataFragment.xhtml
 /opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/se-SE > /opt/dv/Bundle_se.properties
 /opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/sl-SI > /opt/dv/Bundle_sl.properties
 /opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/de-AT > /opt/dv/Bundle_de.properties
