c0fd896751f2965dd12aa50294b004c1842b5156
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-11-06 17:22:11 +0100

    Bundle properties updated for English

--- dataversedock/dataverse-property-files/languageswitch.sh
+++ dataversedock/dataverse-property-files/languageswitch.sh
@@ -12,6 +12,7 @@
 
 if [ -n "$CV_MANAGER_URL" ]; then
     # English
+    echo ""  >> /opt/dv/Bundle.properties
     echo "cvmanagerURL=$CV_MANAGER_URL" >> /opt/dv/Bundle.properties
     echo 'lang=en' >> /opt/dv/Bundle.properties
 
