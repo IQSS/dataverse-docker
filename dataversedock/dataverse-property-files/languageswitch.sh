3410a507ef70b55bba78ecb3dd9ed4faaf4e93b9
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-11-06 16:04:55 +0100

    Languages added to Bundle properties

--- dataversedock/dataverse-property-files/languageswitch.sh
+++ dataversedock/dataverse-property-files/languageswitch.sh
@@ -7,6 +7,36 @@
 /opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/sl-SI > /opt/dv/Bundle_sl.properties
 /opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/de-AT > /opt/dv/Bundle_de.properties
 /opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/fr-FR > /opt/dv/Bundle_fr.properties
+/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/it-IT > /opt/dv/Bundle_it.properties
 /opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/hu-HU > /opt/dv/Bundle_hu.properties
 
-curl http://localhost:8080/api/admin/settings/:Languages -X PUT -d '[{ "locale":"en", "title":"English"}, {"locale":"sl-SI", "title": "Slovenian"},  {"locale":"de-AT", "title": "German"},  {"locale":"fr-FR", "title":"French"}, {  "locale":"se-SE", "title":"Swedish" }]'
+if [ -n "$CV_MANAGER_URL" ] then;
+    # English
+    echo "cvmanagerURL=$CV_MANAGER_URL" >> /opt/dv/Bundle.properties
+    echo 'lang=en' >> /opt/dv/Bundle.properties
+
+    # Swedish
+    echo "cvmanagerURL=$CV_MANAGER_URL" >> /opt/dv/Bundle_se.properties
+    echo 'lang=se' >> /opt/dv/Bundle_se.properties
+
+    # Slovenian
+    echo "cvmanagerURL=$CV_MANAGER_URL" >> /opt/dv/Bundle_sl.properties
+    echo 'lang=sl' >> /opt/dv/Bundle_sl.properties
+
+    # German
+    echo "cvmanagerURL=$CV_MANAGER_URL" >> /opt/dv/Bundle_de.properties
+    echo 'lang=de' >> /opt/dv/Bundle_de.properties
+
+    # French
+    echo "cvmanagerURL=$CV_MANAGER_URL" >> /opt/dv/Bundle_fr.properties
+    echo 'lang=fr' >> /opt/dv/Bundle_fr.properties
+
+    # Italian
+    echo "cvmanagerURL=$CV_MANAGER_URL" >> /opt/dv/Bundle_it.properties
+    echo 'lang=it' >> /opt/dv/Bundle_it.properties
+
+    echo "cvmanagerURL=$CV_MANAGER_URL" >> /opt/dv/Bundle_hu.properties
+    echo 'lang=hu' >> /opt/dv/Bundle_hu.properties
+fi
+
+curl http://localhost:8080/api/admin/settings/:Languages -X PUT -d '[{ "locale":"en", "title":"English"}, {"locale":"sl-SI", "title": "Slovenian"},  {"locale":"de-AT", "title": "German"},  {"locale":"fr-FR", "title":"French"}, {"locale":"it-IT", "title":"Italian"}, {"locale":"hu-HU", "title":"Hungarian"}, {  "locale":"se-SE", "title":"Swedish" }]'
