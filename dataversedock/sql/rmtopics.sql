9883724eba1610da183b064c76b3c076828ef98c
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-12-13 17:32:50 +0100

    Translated title topic changed

--- dataversedock/sql/rmtopics.sql
+++ dataversedock/sql/rmtopics.sql
@@ -1,1 +1,2 @@
 delete from controlledvocabularyvalue where identifier like '%D%' and datasetfieldtype_id=20 and strvalue!='Social Sciences';
+update datasetfieldtype set title='Translated Title' where name='alternativeTitle';
