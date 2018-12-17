0fae1d6f00c9a33d7fb62dccd44ce378e7182121
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-12-17 16:50:23 +0100

    Leaving two topics for DataverseEU

--- dataversedock/sql/rmtopics.sql
+++ dataversedock/sql/rmtopics.sql
@@ -1,2 +1,2 @@
-delete from controlledvocabularyvalue where identifier like '%D%' and datasetfieldtype_id=20 and strvalue!='Social Sciences';
+delete from controlledvocabularyvalue where identifier like '%D%' and datasetfieldtype_id=20 and strvalue!='Arts and Humanities' and strvalue!='Social Sciences';
 update datasetfieldtype set title='Translated Title' where name='alternativeTitle';
