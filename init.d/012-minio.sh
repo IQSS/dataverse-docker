bbe4583fc5f37a03187bd91a11a82108b340c3ad
root <root@localhost>
2021-12-03 10:53:07 +0000

    MinIO startup scripts added

---
+++ init.d/012-minio.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.type=s3"
+asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.label=${minio_label_1}"
+asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.bucket-name=${minio_bucket_1}"
+asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.download-redirect=false"
+asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.url-expiration-minutes=120"
+asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.connection-pool-size=4096"
+asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.profile=${minio_profile_1}"
+asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.custom-endpoint-url=http\:\/\/minio\:9016"
+asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.path-style-access=true"
