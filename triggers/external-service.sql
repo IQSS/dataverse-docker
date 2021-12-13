33bb6b84a2a5f1cfe0596a1ba0660e5d27821ed8
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2021-12-13 13:34:42 +0100

    Trigger to invoke external service(s)

---
+++ triggers/external-service.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE FUNCTION update_datasetversion_notify()
+RETURNS trigger AS
+$BODY$
+
+DECLARE
+data JSONB;
+result JSONB;
+BEGIN
+SELECT json_agg(tmp)  -- requires Postgres9.3+
+INTO data
+FROM (
+-- subquery:
+-- SELECT dvo.id, dvo.identifier, dvo.protocol, dvo.authority, dvo.publicationdate, dsv.versionstate, dsv.versionnumber, dsv.minorversionnumber
+-- FROM dvobject dvo, datasetversion dsv
+-- WHERE dvo.id=dsv.dataset_id AND NEW.versionstate='RELEASED' AND dvo.id = NEW.dataset_id AND OLD.versionstate != NEW.versionstate
+SELECT DISTINCT ON (dvo.id) dvo.id, dvo.identifier, dvo.protocol, dvo.authority, dvo.publicationdate, dsv.versionstate, dsv.versionnumber, dsv.minorversionnumber
+FROM dvobject dvo, datasetversion dsv
+WHERE dvo.id=dsv.dataset_id AND NEW.versionstate='RELEASED' AND dvo.id = NEW.dataset_id
+ORDER BY dvo.id ASC, dsv.versionnumber DESC, dsv.minorversionnumber DESC
+) tmp;
+--result := json_build_object('data', data, 'row', row_to_json(NEW));
+result := json_build_object('data', data);
+PERFORM pg_notify('released_versionstate_datasetversion', result::TEXT);
+RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE
+COST 100;
+ALTER FUNCTION update_datasetversion_notify()
+OWNER TO dvnuser;
+
+CREATE TRIGGER add_versionstate_task_event_trigger
+AFTER UPDATE OF versionstate
+ON datasetversion
+FOR EACH ROW
+EXECUTE PROCEDURE update_datasetversion_notify();
