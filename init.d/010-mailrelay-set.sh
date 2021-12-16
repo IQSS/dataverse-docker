2d9757844dbb819010fc9607b4877b0d6ea8dfb5
Slava Tykhonov <4tikhonov@gmail.com>
2021-12-16 20:29:08 +0100

    smtp settings adjusted

--- init.d/010-mailrelay-set.sh
+++ init.d/010-mailrelay-set.sh
@@ -5,5 +5,5 @@
 if [ "${system_email}" ]; then
     curl -X PUT -d ${system_email} http://localhost:8080/api/admin/settings/:SystemEmail
     asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} delete-javamail-resource mail/notifyMailSession
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-javamail-resource --mailhost ${mailhost} --mailuser ${mailuser} --fromaddress ${no_reply_email} --property mail.smtp.auth=false:mail.smtp.password=${smpt_password}:mail.smtp.port=${smpt_port}:mail.smtp.socketFactory.port=${socket_port}:mail.smtp.socketFactory.fallback=false mail/notifyMailSession
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-javamail-resource --mailhost ${mailhost} --mailuser ${mailuser} --fromaddress ${no_reply_email} --property mail.smtp.auth=false:mail.smtp.password=${smtp_password}:mail.smtp.port=${smtp_port}:mail.smtp.socketFactory.port=${socket_port}:mail.smtp.socketFactory.fallback=false mail/notifyMailSession
 fi
