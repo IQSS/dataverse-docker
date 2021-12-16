4370763e48a90dbbef3a002214273b6dd14b3e19
Slava Tykhonov <4tikhonov@gmail.com>
2021-12-16 20:38:22 +0100

    Defining a flexible storage layer in payara

--- init.d/012-minio-bucket1.sh
+++ init.d/012-minio-bucket1.sh
@@ -3,13 +3,13 @@
 # MinIO bucket activation
 # https://guides.dataverse.org/en/latest/installation/config.html#id87
 if [ "${minio_label_1}" ]; then
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.type=s3"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.label=${minio_label_1}"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.bucket-name=${minio_bucket_1}"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.download-redirect=false"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.url-expiration-minutes=120"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.connection-pool-size=4096"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.profile=${minio_profile_1}"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.custom-endpoint-url=http\:\/\/minio\:9016"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.Minio.path-style-access=true"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_1}.type\=s3"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_1}.label\=${minio_label_1}"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_1}.bucket-name\=${minio_bucket_1}"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_1}.download-redirect\=false"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_1}.url-expiration-minutes\=120"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_1}.connection-pool-size\=4096"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_1}.profile\=${minio_profile_1}"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_1}.custom-endpoint-url\=http\:\/\/minio\:9016"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_1}.path-style-access\=true"
 fi
--- init.d/013-minio-bucket2.sh
+++ init.d/013-minio-bucket2.sh
@@ -3,13 +3,13 @@
 # MinIO bucket 2 activation
 # https://guides.dataverse.org/en/latest/installation/config.html#id87
 if [ "${minio_label_2}" ]; then
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.MinioUiB.type=s3"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.MinioUiB.label=${minio_label_2}"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.MinioUiB.bucket-name=${minio_bucket_2}"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.MinioUiB.download-redirect=false"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.MinioUiB.url-expiration-minutes=120"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.MinioUiB.connection-pool-size=4096"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.MinioUiB.profile=${minio_profile_2}"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.MinioUiB.custom-endpoint-url=http\:\/\/minio\:9016"
-    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "\-Ddataverse.files.MinioUiB.path-style-access=true"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_2}.type\=s3"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_2}.label\=${minio_label_2}"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_2}.bucket-name\=${minio_bucket_2}"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_2}.download-redirect\=false"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_2}.url-expiration-minutes\=120"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_2}.connection-pool-size\=4096"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_2}.profile\=${minio_profile_2}"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_2}.custom-endpoint-url\=http\:\/\/minio\:9016"
+    asadmin --user=${ADMIN_USER} --passwordfile=${PASSWORD_FILE} create-jvm-options "-Ddataverse.files.${bucketname_2}.path-style-access\=true"
 fi
