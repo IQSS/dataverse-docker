b51a2e490d95b323c46bcf8db245a461f01a98e3
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2022-04-14 11:32:58 +0200

    Reservation for CBS vocabulary

--- distros/odissei-init.sh
+++ distros/odissei-init.sh
@@ -7,5 +7,8 @@ curl -I -X POST -H Content-Type:text/turtle -T stw.ttl -G http://0.0.0.0:3030/sk
 curl -L -o unescothes.ttl http://skos.um.es/unescothes/unescothes.ttl
 curl -I -X POST -H Content-Type:text/turtle -T unescothes.ttl -G http://0.0.0.0:3030/skosmos/data --data-urlencode graph=http://skos.um.es/unescothes/
 
+# wget from github and store CBS vocabulary in ./config/cbs-variables-thesaurus.ttl
+curl -I -X POST -H Content-Type:text/turtle -T ./config/cbs-variables-thesaurus.ttl -G http://0.0.0.0:3030/skosmos/data --data-urlencode graph=http://cbs.nl/variables/
+
 echo "Checking search index..."
 curl "http://0.0.0.0:8000/rest/v1/search?vocab=stw&query=a*"
