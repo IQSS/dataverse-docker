2ce6630d932021ce939d4f62bb7d599d63f298a5
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2022-06-02 16:28:14 +0200

    Schema file added for SOLR

--- triggers/metadata-update.sh
+++ triggers/metadata-update.sh
@@ -5,6 +5,7 @@ for schemafile in $1/*.tsv; do
 done
 
 cd /tmp
+wget https://github.com/IQSS/dataverse/releases/download/v5.8/schema.xml
 wget https://dataverse.scholarsportal.info/guides/en/latest/_downloads/4dc30a4612e714fd63732a21e8e7b030/update-fields.sh
 curl "http://localhost:8080/api/admin/index/solr/schema" | bash ./update-fields.sh ./schema.xml
 cp /tmp/schema.xml /opt/payara/dvinstall/schema.xml
