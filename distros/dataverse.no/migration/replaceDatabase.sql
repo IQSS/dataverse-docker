5ab2844330306eb6bc0d9ab255c9ac606343389e
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2022-06-02 11:57:49 +0000

    updated sql statement

--- distros/dataverse.no/migration/replaceDatabase.sql
+++ distros/dataverse.no/migration/replaceDatabase.sql
@@ -1,4 +1,4 @@
-update dvobject set storageidentifier='S3://2002-green-dataversenotest1:' where dtype='Dataset';
+UPDATE dvobject SET storageidentifier=REPLACE(storageidentifier,'file://','S3://') WHERE dtype='Dataset' and storageidentifier like '%file://%';
 
 UPDATE dvobject SET storageidentifier=REPLACE(storageidentifier,'file://','S3://2002-green-dataversenotest1:') WHERE id IN (SELECT o.id FROM dvobject o, dataset s WHERE o.dtype = 'DataFile' AND s.id = o.owner_id AND s.harvestingclient_id IS null AND o.storageidentifier LIKE '%file://%');
 UPDATE dvobject SET storageidentifier=REPLACE(storageidentifier,'local://','S3://2002-green-dataversenotest1:') WHERE id IN (SELECT o.id FROM dvobject o, dataset s WHERE o.dtype = 'DataFile' AND s.id = o.owner_id AND s.harvestingclient_id IS null AND o.storageidentifier LIKE '%file://%');
