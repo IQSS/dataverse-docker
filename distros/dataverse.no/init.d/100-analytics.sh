e88d9a08ed79aa4434e4d303fdf410c6f86795c8
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2022-07-12 09:47:04 +0000

    analytics init update

--- distros/dataverse.no/init.d/100-analytics.sh
+++ distros/dataverse.no/init.d/100-analytics.sh
@@ -1,2 +1,6 @@
 curl -o $DOCROOT_DIR/analytics.xhtml  https://raw.githubusercontent.com/DataverseNO/dataverse-docker/dataverse.no/distros/dataverse.no/modification/analytics.xhtml
-curl -X PUT -d $DOCROOT_DIR/analytics.xhtml http://localhost:8080/api/admin/settings/:WebAnalyticsCode
+
+if [ ! -z "$WEBANALYTICSON" ]
+then
+      curl -X PUT -d $DOCROOT_DIR/analytics.xhtml http://localhost:8080/api/admin/settings/:WebAnalyticsCode
+fi
