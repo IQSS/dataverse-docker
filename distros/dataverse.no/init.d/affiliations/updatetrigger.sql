aa58d547f36899c8e68fe423e78ee1d8df2b1ac5
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2022-07-20 09:52:04 +0000

    updatetrigger.sql

---
+++ distros/dataverse.no/init.d/affiliations/updatetrigger.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS public.dvnoaffiliations (
+    id bigint,
+    dvno_affiliation character varying(255) DEFAULT NULL::character varying,
+    dvno_group_name character varying(255) DEFAULT NULL::character varying,
+    dvno_email_level integer DEFAULT 2
+);
+
+ALTER TABLE public.dvnoaffiliations OWNER TO dataverse;
+
+CREATE OR REPLACE FUNCTION public.affiliationupdate() RETURNS trigger
+     LANGUAGE plpgsql
+     AS $$
+
+BEGIN
+IF NEW.actionsubtype='login' THEN
+    update authenticateduser set affiliation=dvno_affiliation from dvnoaffiliations where dvno_group_name=substring(email, '\S+\W(\w+\W+\w+)') and substring(email, '\S+\W(\w+\W+\w+)') in (select dvno_group_name from dvnoaffiliations) and useridentifier in (select regexp_replace(useridentifier, '@', '') from actionlogrecord where actionsubtype='login' order by starttime desc limit 1);
+    update authenticateduser set affiliation=dvno_affiliation from dvnoaffiliations where dvno_group_name=substring(email, '\S+\W(\w+\W+\w+\W+\w+)') and substring(email, '\S+\W(\w+\W+\w+\W+\w+)') in (select dvno_group_name from dvnoaffiliations) and useridentifier in (select regexp_replace(useridentifier, '@', '') from actionlogrecord where actionsubtype='login' order by starttime desc limit 1);
+END IF
+RETURN NULL;
+END;
+$$
+CREATE TRIGGER affiliation_trigger_actionlog AFTER INSERT ON public.actionlogrecord FOR EACH ROW EXECUTE PROCEDURE public.affiliationupdate();
