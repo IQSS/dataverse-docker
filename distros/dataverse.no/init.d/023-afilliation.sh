cf1ee5f7dcfc6445fdb7839fb9223d6703df0ffb
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2022-07-27 07:37:03 +0000

    Curl comment for Feide affiliation option

--- distros/dataverse.no/init.d/023-afilliation.sh
+++ distros/dataverse.no/init.d/023-afilliation.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -X PUT -d "affiliation" http://localhost:8080/api/admin/settings/:ShibAffiliationAttribute
+curl -X PUT -d True http://localhost:8080/api/admin/settings/ShibAffiliationFeide
 
