7992e09bbc62c337872682e424fab302bfef0892
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2022-07-27 09:13:20 +0000

    fixed affiliation curl

--- distros/dataverse.no/init.d/023-afilliation.sh
+++ distros/dataverse.no/init.d/023-afilliation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -X PUT -d "affiliation" http://localhost:8080/api/admin/settings/:ShibAffiliationAttribute
-curl -X PUT -d True http://localhost:8080/api/admin/settings/ShibAffiliationFeide
+curl -X PUT -d True http://localhost:8080/api/admin/settings/:ShibAffiliationFeide
 
