60e49227a2c7a68ef8c21a31771a2bd7e5c18b46
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2022-08-22 07:35:37 +0000

    added persistant index functionality

--- distros/dataverse.no/init.d/023-afilliation.sh
+++ distros/dataverse.no/init.d/023-afilliation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -X PUT -d "affiliation" http://localhost:8080/api/admin/settings/:ShibAffiliationAttribute
-curl -X PUT -d True http://localhost:8080/api/admin/settings/:ShibAffiliationFeide
-
+curl -X PUT -d "lastAffiliation" http://localhost:8080/api/admin/settings/:ShibAffiliationOrder
+#curl -X PUT -d "firstAffiliation" http://localhost:8080/api/admin/settings/:ShibAffiliationOrder
--- distros/dataverse.no/migration/create-backup-db.sh
+++ distros/dataverse.no/migration/create-backup-db.sh
@@ -3,5 +3,9 @@ docker exec -it  postgres  bash -c  "pg_dump -U dataverse dataverse > /var/lib/p
 gzip -c /extdisk/database-data-demo/dataverse.dump > "/extdisk/database-data-demo/dataverse$(date +'%Y%m%d').dump.gz"
 docker exec -it  postgres  bash -c  "createdb -U dataverse dataverse-tmp"
 docker exec -it  postgres  bash -c  "psql -U dataverse dataverse-tmp -f /var/lib/postgresql/data/dataverse.dump"
-cp ./cleanup-database.sql /var/lib/postgresql/data/
+docker cp ./cleanup-database.sql postgres:/var/lib/postgresql/data/
 docker exec -it  postgres  bash -c  "psql -U dataverse dataverse-tmp -f /var/lib/postgresql/data/cleanup-database.sql"
+docker exec -it  postgres  bash -c  "pg_dump -U dataverse dataverse-tmp > /var/lib/postgresql/data/dataverseCL.dump"
+docker exec -it  postgres  bash -c  "dropdb -U dataverse dataverse-tmp"
+gzip -c /extdisk/database-data-demo/dataverse.dump > "/extdisk/database-data-demo/dataverseCL$(date +'%Y%m%d').dump.gz"
+
