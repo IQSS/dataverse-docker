f99c3973f58557f4960a443136193ffa2f4af98d
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2022-10-03 19:38:18 +0000

    Setup script updated

--- distros/CM2022/setup.sh
+++ distros/CM2022/setup.sh
@@ -4,3 +4,6 @@ git clone https://github.com/apache/superset
 cp superset-docker-compose.yml ./superset/
 cd superset
 docker-compose -f superset-docker-compose.yml up -d 
+cd ../did
+docker-compose up -d
+cd ..
