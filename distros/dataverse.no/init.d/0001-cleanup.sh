416a9fc08d682220b0bd280ebe1c425febb422e2
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2022-12-08 11:42:19 +0000

    fixed docker compose

--- distros/dataverse.no/init.d/0001-cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash/
-if [ -f /mnt/tmp/cleanup.sql ]
-then
-	export PGPASSWORD=`cat /secrets/db/password`
-	psql -U dataverse dataverse -h postgres -f /mnt/tmp/cleanup.sql
-	mv /mnt/tmp/cleanup.sql /mnt/tmp/cleanup.sql-used
-fi
