c3e0b27fd64d7fae28ac3d80e9b30bd5776af1c3
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2023-01-03 07:34:02 +0000

    fixed curation lable (special char not allowed)

--- distros/dataverse.no/init.d/024-curation-lables.sh
+++ distros/dataverse.no/init.d/024-curation-lables.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X PUT -d '{"Standard Process":["Curator Assigned", "In Curation", "Awaiting Reply", "Legal/Ethical Concerns", "Awaiting Final Approval", "In Double-Blind Review", "Awaiting Article Publication", "Candidate for Deletion"], "Alternate Process":["State 1","State 2","State 3"]}' http://localhost:8080/api/admin/settings/:AllowedCurationLabels
+curl -X PUT -d '{"Standard Process":["Curator Assigned", "In Curation", "Awaiting Reply", "Legal or Ethical Concerns", "Awaiting Final Approval", "In Double Blind Review", "Awaiting Article Publication", "Candidate for Deletion"], "Alternate Process":["State 1","State 2","State 3"]}' http://localhost:8080/api/admin/settings/:AllowedCurationLabels
