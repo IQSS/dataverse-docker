0a0b9395cd31ae349f22a0a1e9c15ac3eb210843
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2023-01-06 09:47:58 +0000

    fixed header/footer custumisation

--- distros/dataverse.no/init.d/101-header-footer-custumisation.sh
+++ distros/dataverse.no/init.d/101-header-footer-custumisation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/DataverseNO/dataverse-docker/dataverse.no/distros/dataverse.no/modification/dataverse_header.xhtml -O /tmp/custom-header.xhtml    
-wget https://raw.githubusercontent.com/DataverseNO/dataverse-docker/dataverse.no/distros/dataverse.no/modification/dataverse_footer.xhtml -O /tmp/custom-footer.xhtml
+wget https://raw.githubusercontent.com/DataverseNO/dataverse-docker/dataverse.no/distros/dataverse.no/modification/custom-header.html -O /tmp/custom-header.html    
+wget https://raw.githubusercontent.com/DataverseNO/dataverse-docker/dataverse.no/distros/dataverse.no/modification/custom-footer.html -O /tmp/custom-footer.html
 
 curl -X PUT -d '/logos/navbar/logo.png' http://localhost:8080/api/admin/settings/:LogoCustomizationFile
 curl -X PUT -d '/tmp/custom-header.html' http://localhost:8080/api/admin/settings/:HeaderCustomizationFile
--- distros/dataverse.no/init.d/204-custumisation.sh
+++ distros/dataverse.no/init.d/204-custumisation.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+#curl -X PUT -d '/logos/navbar/logo.png' http://localhost:8080/api/admin/settings/:LogoCustomizationFile
+#curl -X PUT -d '/tmp/custom-header.html' http://localhost:8080/api/admin/settings/:HeaderCustomizationFile
+#curl -X PUT -d '/tmp/custom-footer.html' http://localhost:8080/api/admin/settings/:FooterCustomizationFile
+
+
 wget https://raw.githubusercontent.com/DataverseNO/dataverse-docker/dataverse.no/distros/dataverse.no/modification/loginpage.xhtml -O /tmp/loginpage.xhtml
-wget https://raw.githubusercontent.com/DataverseNO/dataverse-docker/dataverse.no/distros/dataverse.no/modification/dataverse_header.xhtml -O /tmp/dataverse_header.xhtml 
-wget https://raw.githubusercontent.com/DataverseNO/dataverse-docker/dataverse.no/distros/dataverse.no/modification/dataverse_footer.xhtml -O /tmp/dataverse_footer.xhtml 
+#wget https://raw.githubusercontent.com/DataverseNO/dataverse-docker/dataverse.no/distros/dataverse.no/modification/dataverse_header.xhtml -O /tmp/dataverse_header.xhtml 
+#wget https://raw.githubusercontent.com/DataverseNO/dataverse-docker/dataverse.no/distros/dataverse.no/modification/dataverse_footer.xhtml -O /tmp/dataverse_footer.xhtml 
 
 
 cp /tmp/loginpage.xhtml  /opt/payara/appserver/glassfish/domains/domain1/applications/dataverse/loginpage.xhtml
-cp /tmp/dataverse_header.xhtml  /opt/payara/appserver/glassfish/domains/domain1/applications/dataverse/dataverse_header.xhtml
-cp /tmp/dataverse_footer.xhtml  /opt/payara/appserver/glassfish/domains/domain1/applications/dataverse/dataverse_footer.xhtml
+#cp /tmp/dataverse_header.xhtml  /opt/payara/appserver/glassfish/domains/domain1/applications/dataverse/dataverse_header.xhtml
+#cp /tmp/dataverse_footer.xhtml  /opt/payara/appserver/glassfish/domains/domain1/applications/dataverse/dataverse_footer.xhtml
 
