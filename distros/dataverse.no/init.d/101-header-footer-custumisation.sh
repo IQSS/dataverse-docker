d252331c721918f53c470caa178686e5d5e3dc51
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2023-01-17 11:45:43 +0000

    removed minio /updated footer/header

--- distros/dataverse.no/init.d/101-header-footer-custumisation.sh
+++ distros/dataverse.no/init.d/101-header-footer-custumisation.sh
@@ -7,3 +7,5 @@ curl -X PUT -d '/logos/navbar/logo.png' http://localhost:8080/api/admin/settings
 curl -X PUT -d '/tmp/custom-header.html' http://localhost:8080/api/admin/settings/:HeaderCustomizationFile
 curl -X PUT -d '/tmp/custom-footer.html' http://localhost:8080/api/admin/settings/:FooterCustomizationFile
 curl -X PUT -d http://site.uit.no/dataverseno/deposit/ http://localhost:8080/api/admin/settings/:GuidesBaseUrl
+curl -X PUT -d '' http://localhost:8080/api/admin/settings/:GuidesVersion
+curl -X PUT -d https://site.uit.no/dataverseno/support/ http://localhost:8080/api/admin/settings/:NavbarSupportUrl
