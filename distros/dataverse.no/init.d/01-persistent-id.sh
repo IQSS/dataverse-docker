d0b099bf2062a3f98adddecf9f475fe75a07c866
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2023-01-23 10:24:43 +0000

    updated navbar url

--- distros/dataverse.no/init.d/01-persistent-id.sh
+++ distros/dataverse.no/init.d/01-persistent-id.sh
@@ -3,7 +3,9 @@ echo "Setting up the settings" >> /tmp/status.log
 echo  "- Allow internal signup" >> /tmp/status.log
 SERVER=http://${DATAVERSE_URL}/api
 echo $SERVER
-curl -X PUT -d yes "$SERVER/admin/settings/:AllowSignUp"
+curl -X PUT -d https://site.uit.no/dataverseno/support/  "$SERVER/admin/settings/:NavbarSupportUrl"
+curl -X PUT -d http://site.uit.no/dataverseno/deposit/ "$SERVER/admin/settings/:NavbarGuidesUrl"
+curl -X PUT -d 'false' "$SERVER/admin/settings/:AllowSignUp"
 curl -X PUT -d /dataverseuser.xhtml?editMode=CREATE "$SERVER/admin/settings/:SignUpUrl"
 curl -X PUT -d CV "$SERVER/admin/settings/:CV"
 curl -X PUT -d burrito $SERVER/admin/settings/BuiltinUsers.KEY
