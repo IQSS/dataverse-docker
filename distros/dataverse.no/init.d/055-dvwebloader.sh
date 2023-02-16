9c5639a056e54877821998e8fbc6e6a7089780be
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2023-02-16 10:29:28 +0000

    updated to internal dvwebloader for 5.13

--- distros/dataverse.no/init.d/055-dvwebloader.sh
+++ distros/dataverse.no/init.d/055-dvwebloader.sh
@@ -1,37 +1,4 @@
 #!/bin/bash
+curl -X PUT -d 'native/http,dcm/rsync+ssh,dvwebloader' http://localhost:8080/api/admin/settings/:UploadMethods
+curl -X PUT -d 'https://gdcc.github.io/dvwebloader/src/dvwebloader.html' http://localhost:8080/api/admin/settings/:WebloaderUrl
 
-export PGPASSWORD=`cat /secrets/db/password`
-psql -U dataverse dataverse -h postgres  -f ${INIT_SCRIPTS_FOLDER}/affiliations/webloader_check.sql -o /tmp/output
-EXIST=`grep Dataverse /tmp/output`
-
-wget https://github.com/DataverseNO/dvwebloader/archive/refs/heads/main.zip -O /tmp/dvwebloader.zip
-unzip -o /tmp/dvwebloader.zip -d $DOCROOT_DIR/logos
-
-if [[ -z $EXIST ]]; then
-echo "Loaded"
-curl -X POST -H 'Content-type: application/json' http://localhost:8080/api/admin/externalTools -d \
-"{
-  \"displayName\": \"Dataverse WebLoader\",
-  \"description\": \"Upload all  the files in a local directory!\",
-  \"toolName\": \"dvwebloader\",
-  \"scope\": \"dataset\",
-  \"contentType\":\"text/plain\",
-  \"types\": [
-    \"explore\"
-  ],
-  \"toolUrl\": \"https://${hostname}/logos/dvwebloader-main/src/dvwebloader.html\",
-  \"toolParameters\": {
-    \"queryParameters\": [
-      {
-        \"siteUrl\": \"{siteUrl}\"
-      },
-      {
-        \"datasetPid\": \"{datasetPid}\"
-      },
-      {
-        \"key\": \"{apiToken}\"
-      }
-    ]
-  }
-}"
-fi
