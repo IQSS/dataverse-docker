bb4fb04310cfd16b560e33f7d9bac1ea89dd4ad0
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2023-02-21 08:21:14 +0000

    update to 5.13

--- distros/dataverse.no/init.d/055-dvwebloader.sh
+++ distros/dataverse.no/init.d/055-dvwebloader.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -X PUT -d 'native/http,dcm/rsync+ssh,dvwebloader' http://localhost:8080/api/admin/settings/:UploadMethods
+curl -X PUT -d 'native/http,dvwebloader' http://localhost:8080/api/admin/settings/:UploadMethods
 curl -X PUT -d 'https://gdcc.github.io/dvwebloader/src/dvwebloader.html' http://localhost:8080/api/admin/settings/:WebloaderUrl
 
