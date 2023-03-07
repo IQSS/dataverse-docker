1f495da3fe904b241c1337b8c2f0cc2f7bb7766f
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2023-03-07 10:29:44 +0000

    added backup of draft

--- distros/dataverse.no/init.d/cronjob/backupfiles.sql
+++ distros/dataverse.no/init.d/cronjob/backupfiles.sql
@@ -1,7 +1,10 @@
 --select storageidentifier from dvobject where modificationtime>='2022-12-05';
 
 
-select storageidentifier, CONCAT(authority, '/',split_part(identifier, '/', 1) , '/', REPLACE(storageidentifier, 'S3://2002-green-dataversenotest1:','')), id from dvobject where storageidentifier like '%S3:%' and dtype='DataFile' and modificationtime>=(current_date - INTERVAL '1 day');
+--select storageidentifier, CONCAT(authority, '/',split_part(identifier, '/', 1) , '/', REPLACE(storageidentifier, 'S3://2002-green-dataversenotest1:','')), id from dvobject where storageidentifier like '%S3:%' and dtype='DataFile' and modificationtime>=(current_date - INTERVAL '1 day');
+
+
+select storageidentifier, CONCAT( get_authority(id), '/', get_identifier(id), '/', REPLACE(storageidentifier, 'S3://2002-green-dataversenotest1:','')) from dvobject where storageidentifier like '%S3:%' and dtype='DataFile' and modificationtime>=(current_date - INTERVAL '7 day');
 
 
 
