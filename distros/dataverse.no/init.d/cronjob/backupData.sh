ce875fcc8dec0c2d449039b5f778cf0d70d37162
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2023-03-28 09:43:21 +0000

    fixed fine name in S3

--- distros/dataverse.no/init.d/cronjob/backupData.sh
+++ distros/dataverse.no/init.d/cronjob/backupData.sh
@@ -5,7 +5,7 @@ cp -r /secrets/aws-cli/.aws ~
 
 #file=10.21337/OZ4JBV/1869225dfbd-4edecc03da9e
 
-files=`psql -U dataverse dataverse -h postgres  -f ${INIT_SCRIPTS_FOLDER}/cronjob/backupfiles.sql | grep S3 | awk '{split($0,a,"|"); print a[2]}'`
+files=`psql -U dataverse dataverse -h postgres  -f ${INIT_SCRIPTS_FOLDER}/cronjob/backupfiles.sql | grep S3 | awk '{split($0,a,"|"); print a[2]}' | sed "s/S3:\/\/$aws_bucket_name://"`
 
 for  file in $files
 	do 	
