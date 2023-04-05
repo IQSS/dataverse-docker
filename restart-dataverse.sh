76e6e9d40612db52678b6de53c8c802c76a0d130
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2023-04-05 09:41:39 +0000

    fixed restart feature

--- restart-dataverse.sh
+++ restart-dataverse.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
+if [ $# -eq 0 ]
+	then
+		echo "Usage : restart-dataverse.sh [dataverse adress] [distrib location(optional)]"
+		exit 1
+	fi
 
 # Check if Dataverse is online
 healthcheck="/tmp/healthcheck.log"
 restartpid='/tmp/restart.pid'
 rm $healthcheck
 DATAVERSE=$1 #'https://test-docker.dataverse.no'
+DISTRIB=${2:-'/distrib'}
+DELAY=15
 echo $DATAVERSE
 
 curl -s ${DATAVERSE}/api/dataverses/root|grep "name" >> $healthcheck
-DELAY=15
+
+cat $healthcheck
+
+if test "`find $restartpid -mmin +15 -print 2>/dev/null`"
+	then
+		echo "${restartpid} is too old, deleting, this may lead to an other restart"
+		rm $restartpid
+     	fi	     
+
 
 if [ -s $healthcheck ];
 then
@@ -22,9 +37,12 @@ else
         else
 	     echo 'restarting...' > $restartpid
 	     date >> /mntblob/logs/restart.log
-	     cd /distrib/dataverse-docker
+	     cd ${DISTRIB}/dataverse-docker
+	     echo "down"
              /usr/local/bin/docker-compose down
+	     echo "waiting ${DELAY}s for ${DATAVERSE} to go down"
              sleep $DELAY
+	     echo "up"
              /usr/local/bin/docker-compose up -d
 	fi
 fi
