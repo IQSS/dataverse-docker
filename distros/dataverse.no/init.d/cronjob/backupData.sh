d623ae717be464f385a48b4f78949af57b07a5ce
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2023-04-28 12:55:04 +0000

    fixed database backup to s3

--- distros/dataverse.no/init.d/cronjob/backupData.sh
+++ distros/dataverse.no/init.d/cronjob/backupData.sh
@@ -6,6 +6,9 @@ cp -r /secrets/aws-cli/.aws ~
 #file=10.21337/OZ4JBV/1869225dfbd-4edecc03da9e
 
 files=`psql -U dataverse dataverse -h postgres  -f ${INIT_SCRIPTS_FOLDER}/cronjob/backupfiles.sql | grep S3 | awk '{split($0,a,"|"); print a[2]}' | sed "s/S3:\/\/$aws_bucket_name://"`
+dump=`ls /data/databaseDumps/ -Art | tail -n 1`
+
+aws s3 --endpoint https://$aws_endpoint cp /data/databaseDumps/$dump s3://$aws_bucket_name/databaseDumps
 
 for  file in $files
 	do 	
--- distros/dataverse.no/runOnce/crontab.sh
+++ distros/dataverse.no/runOnce/crontab.sh
@@ -6,3 +6,6 @@ cp -r /distib/private/.ssh /var/opt/microsoft/omsagent/run
 chown -R omsagent /var/opt/microsoft/omsagent/run/.ssh
 #0 0 * * * /usr/bin/docker exec dataverse "/opt/payara/init.d/cronjob/backupData.sh" >> /var/log/bakupslogs.log
 #0 0 * * * /usr/bin/docker exec dataverse "/opt/payara/init.d/cronjob/backupData.sh" >> /var/log/bakupslogs.log
+#1 0 * * * /distrib/dataverse-docker/distros/dataverse.no/init.d/cronjob/dumpdatabase.sh
+#*/3 * * * * /bin/bash /distrib/dataverse-docker/restart-dataverse.sh https://dataverse.no
+
