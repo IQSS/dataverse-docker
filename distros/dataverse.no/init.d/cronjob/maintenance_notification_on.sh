ccbfd4dd6912c7ea631408d8020d46cd884fe4b2
Louis-wr <85620187+Louis-wr@users.noreply.github.com>
2023-05-31 13:03:32 +0000

    updatede cronjob

--- distros/dataverse.no/init.d/cronjob/maintenance_notification_on.sh
+++ distros/dataverse.no/init.d/cronjob/maintenance_notification_on.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
 curl -H "Content-type:application/json" -X POST http://localhost:8080/api/admin/bannerMessage --upload-file ${INIT_SCRIPTS_FOLDER}/cronjob/maintenance_notification.json
+
+# 0 16 * * 3 /usr/bin/docker exec dataverse "/opt/payara/init.d/cronjob/maintenance_notification_on.sh"
