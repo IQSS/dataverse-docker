c31f3739ce152e8b82d02dd7df01ff6c017f01bc
Benedikt Meier <benedikt.meier@uit.no>
2024-03-15 08:41:00 +0100

    create directory if not exists and put output to log file

--- distros/dataverse.no/init.d/cronjob/checkETagByFiles.sh
+++ distros/dataverse.no/init.d/cronjob/checkETagByFiles.sh
@@ -3,6 +3,18 @@
 export PGPASSWORD=`cat /secrets/db/password`
 cp -r /secrets/aws-cli/.aws ~
 
+# 
+LogDir="/opt/payara/appserver/glassfish/domains/domain1/logs/"
+if [ ! -d "${LogDir}" ]; then
+	LogDir="/var/log/"
+fi
+
+if [ ! -d "/tmp/" ]; then
+	 mkdir -p "/tmp/"
+fi
+
+LogFile="${LogDir}checkETag_`date +%Y%m%d_%H%M%z`.log"
+
 psql -h ${DATAVERSE_DB_HOST} -U ${DATAVERSE_DB_USER} ${POSTGRES_DATABASE} -f ${INIT_SCRIPTS_FOLDER}/cronjob/checkfiles.sql | grep S3 | awk '{split($0,a,"|"); print a[2] a[3]}' | sed "s/S3:\/\/$aws_bucket_name://" > /tmp/dataverse_checkETag.txt
 
 while read p; do
@@ -13,11 +25,11 @@ while read p; do
 
 
 	if [ -z "${s3ETag}" ]; then
-		echo "is not exist in the s3 storage: ${arrayData[0]}"
+		echo "is not exist in the s3 storage: ${arrayData[0]}" > ${LogFile}
 	else
 
 		if [ "${s3ETag}" != "${arrayData[1]}" ]; then
-			echo "is not equal: ${arrayData[0]}"
+			echo "is not equal: ${arrayData[0]}" > ${LogFile}
 		fi
 	fi
 
