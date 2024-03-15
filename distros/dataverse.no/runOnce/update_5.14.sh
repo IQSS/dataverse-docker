06b177360fd48b91c615e0b3139626617213e591
Benedikt Meier <benedikt.meier@uit.no>
2024-03-15 08:55:05 +0100

    add output update 5.14

--- distros/dataverse.no/runOnce/update_5.14.sh
+++ distros/dataverse.no/runOnce/update_5.14.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "Enable File PID:"
 curl -X PUT -d 'true' http://localhost:8080/api/admin/settings/:FilePIDsEnabled
+echo "PID finish"
 
+echo "Downlaod and load citation.tsv file"
 curl -s https://github.com/IQSS/dataverse/releases/download/v5.14/citation.tsv -o /tmp/citation.tsv
 curl http://localhost:8080/api/admin/datasetfield/load -X POST --data-binary @/tmp/citation.tsv -H "Content-type: text/tab-separated-values"
+echo "citation.tsv finish"
 
 rm /tmp/citation.tsv
